5a25364986b20414dce034ca43870c74b3c64d0d
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-09 12:57:37 +0500

    navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Link, Flex } from "@chakra-ui/core";
+import NextLink from "next/link";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>
+        <NextLink href="/login">
+          <Link color="white" mr={4}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white">Register</Link>
+        </NextLink>
+      </Box>
+    </Flex>
+  );
+};
