0d47a859e7b50732c047f163d9c8a5a56d76f5f5
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-09 13:16:43 +0500

    log out

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Box, Link, Flex } from "@chakra-ui/core";
+import { Box, Link, Flex, Button } from "@chakra-ui/core";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link color="white" mr={4}>
             Login
@@ -16,7 +22,20 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         <NextLink href="/register">
           <Link color="white">Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={4}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
